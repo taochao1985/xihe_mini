998e33299358c3eb269ea04a6f11ff65ac703990
taochao1985 <legendtao1985@gmail.com>
2017-12-26 14:54:32 +0800

    updatE

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var xihe = require('./utils/request.js');
 //app.js
 App({
     globalData: {
-        userInfo : '',
+        userinfo : '',
         openid   : '',
         uid      : 0 
     },
@@ -12,18 +12,16 @@ App({
         }else{
             wx.setStorageSync('agent_uid', 0);
         }
+
         // 登录
-        xihe._get_openid_callback =function(item,data){ 
-            wx.setStorageSync('openid',data.openid);
-            wx.setStorageSync('uid', data.uid) ;
-            if( data.userinfo ){
-                wx.setStorageSync('userinfo', JSON.parse(data.userinfo));
-            } 
-            
-            xihe._getSetting(item);
+        xihe._get_openid_callback =function(data){  
+            var app               = getApp();
+            app.globalData.openid = data.openid;
+            app.globalData.uid    = data.uid;
+            xihe._getSetting(app);
         }; 
 
-        xihe._login = function(item){ 
+        xihe._login = function(){ 
             wx.login({
                 success: res => {
                     // 发送 res.code 到后台换取 openId, sessionKey, unionId
@@ -35,29 +33,37 @@ App({
                             agent_uid: wx.getStorageSync('agent_uid')
                         },
                         callback: function (data) {
-                            xihe._get_openid_callback(item, data);
+                            xihe._get_openid_callback(data);
                         }
                     });
                 }
             })
         }; 
 
-        xihe._getUserInfo = function(item){
+        xihe._setUserInfo = function(app,data){
             
+            app.globalData.userinfo = JSON.parse(data.data.userinfo);
+        };
+
+        xihe._getUserInfo = function(app){
+            if ( !app ){
+                app = getApp();
+            }
             wx.getUserInfo({
                 success: res => {
                     // 可以将 res 发送给后台解码出 unionId 
-                    if (!wx.getStorageSync('openid')){
+                    console.log(app);
+                    if (app.globalData.openid){ 
                         var submitData = {
-                            openid: wx.getStorageSync('openid'),
-                            userinfo: res.rawData,
-                            encrypteddata: res.encryptedData
+                            openid        : app.globalData.openid,
+                            userinfo      : res.rawData,
+                            encrypteddata : res.encryptedData
                         };
                         xihe.post({
                             url: "/api/wechat/update_userinfo",
                             data: submitData,
-                            callback: function (data) {
-                                wx.setStorageSync('userinfo', JSON.parse(data.data.userinfo));
+                            callback: function (data) {  
+                                wx._setUserInfo(app, data); 
                             }
                         });
                     }else{
@@ -66,50 +72,50 @@ App({
 
                     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
                     // 所以此处加入 callback 以防止这种情况
-                    if (item.userInfoReadyCallback) {
-                        item.userInfoReadyCallback(res)
+                    var app = getApp();
+                    if ( app.userInfoReadyCallback ) {
+                        app.userInfoReadyCallback(res)
                     }
                 }
             })
         };
         
-        xihe._getSetting = function(item){
+        xihe._getSetting = function(app){ 
             // 获取用户信息
             wx.getSetting({
-                success: res => { 
-                    if (!res.authSetting['scope.userInfo']) {
-                        wx.authorize({
-                            scope: 'scope.userInfo',
-                            success(res) {
-                                console.log(res);
-                                xihe._getUserInfo(item);
-                            },
-                            fail(res){
-                                console.log(res);
-                            }
-                        })
+                success: res => {  
+                    if (!res.authSetting['scope.userInfo']) { 
+                        console.log('A');
+                        xihe._getUserInfo(app); 
+                        
+                    }else{
+                         
                     }
 
-                    if (res.authSetting['scope.userInfo']) { 
+                    if (res.authSetting['scope.userInfo']) {  
                         // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-                        xihe._getUserInfo(item);
+                        console.log('B');
+                        xihe._getUserInfo(app);
+                       
                     }
+                },
+                fail: res => {
+                    console.log(res);
                 }
             })
         };
  
         wx.checkSession({
             success: res => {  
-                if (!wx.getStorageSync('openid')){ 
-                    xihe._login(this);
-                } else if (!wx.getStorageSync('userInfo')){
+                if ( !this.globalData.openid ){ 
+                    xihe._login();
+                } else if ( !this.globalData.userinfo ){
                     xihe._getSetting(this);
                 }
-                //session 未过期，并且在本生命周期一直有效
             },
             fail: function (data) { 
-                xihe._login(this);
-                return false;
+                console.log('not login');
+                xihe._login();
             }
         })
        
